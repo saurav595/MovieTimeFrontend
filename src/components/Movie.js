a3bd2ab53a830b14f655234bd025a89193d66b93
Saurav Shah <shah.saur@northeastern.edu>
2022-07-01 02:06:53 -0400

    finalizing hw5

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -104,8 +104,6 @@ const Movie = ({user}) => {
                                                 })
                                             }
                                             }
-                                            
-                                            
                                             >
                                                 Delete
                                             </Button>
